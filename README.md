3b55d81fdab31e8e7febbef7fdb67344dfd0cc23
João Pereira <jtopereira@gmail.com>
2021-03-23 23:49:09 +0000

    adding some script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # css_generator
 Website to generate css box and text shadows codes
+
+## To do
+[ ] Implement cache system
+    - store colors, config and code values
+    - store the dark preference
+[ ] Transitions animations
+[ ] Dark Mode/ White Mode
+[ ] Icons effects
+[ ] Responsive for smaller medias 
+[ ] Copy button
--- public/script.js
+++ public/script.js
@@ -1,50 +1,62 @@
 //consts
-const values = ["Shadow Color","Horizontal-offset","Vertical-offset","Blur","Spread","Opacity","Box Color","Background Color"]
+const values = ["Shadow Color", "Horizontal-offset","Vertical-offset","Blur","Spread","Opacity"]
 const box = document.querySelector('#box');
 const background_box = document.querySelector('#background_box')
 const code = document.querySelector("#code")
+const colors = document.querySelector("#colors").children
+const scripts = document.querySelectorAll(".script")
 
 //selectors
 //color + horizontal + vertical + blur + spread
 box.style.boxShadow = 'rgba(0,0,0,0.42) 41px -2px 43px 0px';
-box.style.background = '#777777;'
+box.style.background = '#777777'
 background_box.style.background = 'rgb(59, 59, 59)';
+colors[0].value = "#000000"
+colors[1].value = "#777777"
+colors[2].value = "#3b3b3b"
+ChangeCode("41px -2px 43px 0px rgba(0,0,0,0.42)")
 
-document.querySelector("#levels").querySelectorAll('input').forEach(input => input.addEventListener('change', e => InputChange(e)))
+document.querySelector("#levels").querySelectorAll('input').forEach(input => input.addEventListener('input', e => InputChange(e)))
+document.querySelector("#colors").querySelectorAll('input').forEach(input => input.addEventListener('input', e => ChangeColor(e)))
 
 
 function InputChange(e){
     //select type
-    let mainInput = e.target.previousElementSibling.tag === "p" ? e.target.previousElementSibling.previousElementSibling : e.target
-    let index = values.indexOf(mainInput.previousElementSibling.innerHTML)
+    let mainInput = e.target.parentElement.tagName === "DIV" ? e.target : e.target.parentElement.parentElement.nextElementSibling
+    let index = values.indexOf(mainInput.previousElementSibling.children[0].innerHTML)
     
-    if(mainInput.type === "color"){
-        switch(mainInput.name){
-            case "shadow": //continue this
-        }
-    }
-    else if(values[index] === "Opacity") ChangeOpacityProperty();
-    else if(!values[index].includes("Color")){
-        //non color input
-        ChangeShadowProperty(index, mainInput)
-    }else{
-        //color input
-        if(values[index] === "Shadow Color") ChangeShadowBackground();
-        if(value[index] === "Box Color") box.style.background = mainInput.value;
-        if(value[index] === "Background Color") background_box.style.background = mainInput.value;
+    ChangeShadowProperty(index, mainInput)
+
+}
+function ChangeColor(e){
+    switch(e.target.name){
+        case "shadow":
+            
+            break;
+        case "box":
+            box.style.background = e.target.value
+            break;
+        case "background": background_box.style.background = e.target.value;
     }
 }
-
-function ChangeShadowProperty(index, input){
+function ChangeShadowProperty(index, input, value = input.value){
     let splashIndex = box.style.boxShadow.indexOf(")")
     let old_style = [box.style.boxShadow.slice(0, splashIndex + 1), ...box.style.boxShadow.slice(splashIndex + 2).split(" ")] //box.style.boxShadow.split(" ")
-    old_style[index] = input.value + "px"
+    console.log(old_style)
+    if(values[index] === "Opacity"){
+        old_style[0] = old_style[0].split(",")
+        old_style[0].pop()
+        old_style[0].push(value + ")")
+        old_style[0] = old_style[0].join(", ")
+    }else old_style[index] = value + "px"
+
     let new_style = old_style.join(" ")
     box.style.boxShadow = new_style
     //for webkit etc...
 
-    input.nextElementSibling.nextElementSibling.value = input.value
+    input.previousElementSibling.children[1].children[0].value = value
     ChangeCode(new_style)
+    box.style.boxShadow = new_style
 }
 
 function ChangeOpacityProperty(){
@@ -55,9 +67,5 @@ function ChangeShadowBackground(){
 }
 
 function ChangeCode(new_style){
-    code.innerHTML = `
-        <p>box-shadow: ${new_style};</p>
-        <p>-moz-box-shadow: ${new_style}</p>
-        <p>-webkit-box-shadow: ${new_style}</p>
-    `
+    scripts.forEach(script => script.innerHTML = new_style)
 }
